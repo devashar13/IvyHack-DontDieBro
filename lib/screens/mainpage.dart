f7df4d6908a3304fe19ed1c1bb262945b3d4c054
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2020-10-04 14:45:36 +0530

    Added geolocation

--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:location/location.dart';
 
 class MainPage extends StatefulWidget {
   static const String id = 'mainPage';
@@ -8,24 +10,45 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  Location location = new Location();
+
+  void setUpPositionLocator() async {
+    var pos = await location.getLocation();
+    print(pos);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Main Page'),
-      ),
-      body: Center(
-        child: MaterialButton(
-          onPressed: () {
-            DatabaseReference dbref =
-                FirebaseDatabase.instance.reference().child('Test');
-            dbref.set('Connected');
-          },
-          height: 50,
-          minWidth: 300,
-          color: Colors.green,
+        appBar: AppBar(
+          title: Text('Main Page'),
         ),
-      ),
-    );
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 150),
+            child: GestureDetector(
+              onTap: () {
+                setUpPositionLocator();
+              },
+              child: ClipOval(
+                child: Container(
+                  color: Colors.red[400],
+                  height: 150,
+                  width: 150,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 5),
+                      child: Text(' SOS',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25)),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
